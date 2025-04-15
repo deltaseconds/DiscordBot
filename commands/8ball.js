e367f28f7a35e06d93245ca765b32f3c7ca48504
delta <60275031+deltaseconds@users.noreply.github.com>
2025-04-15 15:27:55 +0200

    Added Clear command and 8ball command, clear command still needs work and 8ball needs more questions

---
+++ commands/8ball.js
@@ -0,0 +1,78 @@
+module.exports = {
+    name: '8ball',
+    description: 'Ask the magic 8ball a question',
+    execute(message, args) {
+       if(!args[0]) {
+        return message.channel.send("Ask a question, will you?");
+       } 
+       const responses = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes, definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful",
+        "No",
+        'FUCK NO',
+        'NO',
+        'NOPE',
+        'NOPE NOPE NOPE',
+        'potentially',
+        'maybe',
+        'probably',
+        'probably not',
+        'probably yes',
+        'in a distant time maybe',
+        'in a distant time yes',
+        'in a distant time no',
+        'in a distant time potentially',
+        'in a distant time maybe not',
+        'in a distant time maybe yes',
+        'in a distant time potentially yes though it is still highly unlikely in every case and it is not recommended to rely on it',
+        'nein',
+        'nein nein nein',
+        'はい、確かに', // Yes, certainly
+        'その通りです', // That's right
+        '間違いなく', // Without a doubt
+        'もちろんです', // Of course
+        'そう思います', // I think so
+        'たぶんそうです', // Probably so
+        'いいえ', // No
+        'だめです', // No good
+        'それは違います', // That's wrong
+        'あり得ません', // Impossible
+        'わかりません', // I don't know
+        'もう一度聞いてください', // Please ask again
+        '後で聞いてください', // Ask me later
+        '今は答えられません', // Can't answer now
+        'それは秘密です', // That's a secret
+        'まだ分かりません', // Don't know yet
+        'おそらく', // Perhaps
+        '可能性はあります', // There's a possibility
+        '期待しないでください', // Don't count on it
+        '見込みは薄いです', // Prospects are slim
+        'いつか分かるでしょう', // You'll know someday
+        'それは運次第です', // It depends on luck
+        'まだ時期尚早です', // It's too early to tell
+        'それは難しいですね', // That's difficult
+        'がんばって！', // Good luck!
+       ]
+       const response = responses[Math.floor(Math.random() * responses.length)];
+       message.channel.send(response);
+    }
+
+}
---
+++ commands/clear.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'clear',
+    description: 'Clear a number of messages',
+    permission: 'MANAGE_MESSAGES',
+    execute(message, args) {
+        if(!message.member.permissions.has('MANAGE_MESSAGES') || !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**ERROR**: You do not have permission to use this command.');
+       if (!args[0]) return message.channel.send('**ERROR**: Please specify the number of messages to delete.');
+       if (isNaN(args[0])) return message.channel.send('**ERROR**: Please provide a valid number.');
+       if (args[0] > 100) return message.channel.send('**ERROR**: You cannot delete more than 100 messages at once.');
+       if (args[0] < 1) return message.channel.send('**ERROR**: You must delete at least 1 message.');
+       message.channel.bulkDelete(args[0]).then(() => {
+        //    message.channel.send(`**SUCCESS**: Cleared ${args[0]} messages.`).then(msg => {
+        //        msg.delete({ timeout: 5000 });
+        //    });
+           message.channel.messages.fetch({ limit: args[0] }).then(messages => {
+              messages.forEach(element => {
+                console.log(element); // log the message object t
+                message.delete(element);
+              }); 
+           })
+       });
+    }
+}
