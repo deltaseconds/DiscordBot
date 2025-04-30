d434bd82f503241ffe155e5acd5f69709f8c51aa
delta <60275031+deltaseconds@users.noreply.github.com>
2025-04-30 12:32:34 +0000

    feat(guildMemberAdd): enhance role assignment logic and permission checks

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,7 +5,10 @@ module.exports = {
         const joinRoles = await db.query(`SELECT * FROM roles WHERE guildID = ?`, member.guild.id);
         if(joinRoles.length == 0) return;
         joinRoles.forEach(role => {
-            let welcomeRole = member.guild.roles.cache.find(rol => rol.id === role.role_id);
+            let welcomeRole = member.guild.roles.cache.find(rol => rol.id === role.role_id) ? member.guild.roles.cache.find(rol => rol.id === role.role_id) : null;
+            if(!welcomeRole) return;
+            if(!member.guild.me.permissions.has("MANAGE_ROLES")) return member.send('**ERROR**: I do not have permission to manage roles.');
+            if(member.roles.cache.has(welcomeRole.id)) return;
             member.guild.roles.add(welcomeRole);
         });
     }
