26971361645550126b186c0f3d1693839d650fb5
delta <60275031+deltaseconds@users.noreply.github.com>
2025-05-02 11:56:33 +0000

    feat(roles): add 'list' functionality to display join roles; update usage message for clarity; added guild purge in case of guild deletion

---
+++ events/guildRemove.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'guildRemove',
+    once: true,
+    async execute(guild) {
+        const db = require('../handlers/mysqlhandler');
+        await db.query('DELETE FROM guilds WHERE id = ?', [guild.id]);
+        await db.query('DELETE FROM levels WHERE guild_id = ?', [guild.id]);
+        await db.query('DELETE FROM levels_settings WHERE guild_id = ?', [guild.id]);
+        await db.query('DELETE FROM levelup_embed_fields WHERE guild_id = ?', [guild.id]);
+        await db.query('DELETE FROM roles WHERE guild_id = ?', [guild.id]);
+        console.log(`Removed ${guild.name} from the database`);
+    }
+}
